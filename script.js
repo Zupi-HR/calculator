33cca92041310b53d42863a14e47148731eb6721
Zupi <dubravac.neven@gmail.com>
2023-03-14 00:25:42 +0100

    računske operacije su gotove kad se radi o 2 broja

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const tipka = document.querySelectorAll('.tipka');
+let sadržajEkrana = document.querySelector('.sadržajEkrana');
+
+let firstandSecondNum = [];
+let operator;
+
 const mathOperations = {
     add: (num1, num2) => num1 + num2,
     subtract: (num1, num2) => num1 - num2,
@@ -7,10 +13,57 @@ const mathOperations = {
 
 
 function operate(operator, num1, num2) {
-  if (operator === "+") return mathOperations.add(num1, num2);
-  else if (operator  === "-") return mathOperations.subtract(num1, num2);
-  else if (operator === "*") return mathOperations.multiply(num1, num2);
-  else if (operator === "/") return mathOperations.divide(num1, num2);
+    if (operator === "+") return mathOperations.add(num1, num2);
+    else if (operator === "-") return mathOperations.subtract(num1, num2);
+    else if (operator === "*") return mathOperations.multiply(num1, num2);
+    else if (operator === "/") return mathOperations.divide(num1, num2);
 }
+
+function getNumberFromDisplay(pozicija) {
+    // uzmi tekst s display, ukloni bilo šta osim brojeva i pretvori u number pa stavi u array
+    const textContentDisplay = sadržajEkrana.textContent;
+    const numericValue = textContentDisplay.match(/\d+(\.\d+)?/g).map(Number);
+    // pretvoreni broj koji je u array stavi u postojeci array s pozicije 
+    firstandSecondNum.push(numericValue[pozicija]);
+}
+
+tipka.forEach(function (tipka) {
+    tipka.addEventListener('click', function (e) {
+        btnValue = e.target.dataset.value;
+        populateDisplay(btnValue);
+        if (e.target.dataset.value == "+") {
+            operator = '+';
+            getNumberFromDisplay(0);
+        }
+
+        else if (e.target.dataset.value == "-") {
+            operator = '-';
+            getNumberFromDisplay(0);
+
+        }
+
+        else if (e.target.dataset.value == "*") {
+            operator = '*';
+            getNumberFromDisplay(0);
+        }
+
+        else if (e.target.dataset.value == '/') {
+            operator = '/';
+            getNumberFromDisplay(0);
+        }
+
+        if (e.target.dataset.value == "=") {
+            getNumberFromDisplay(1);
+            const result = operate(operator, firstandSecondNum[0], firstandSecondNum[1]);
+            populateDisplay(result);
+        }
+
+    })
+})
+
+function populateDisplay(sadržaj) {
+    sadržajEkrana.textContent += sadržaj;
+}
+
 
 
